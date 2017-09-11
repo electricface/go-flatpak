2d4166fd59b8861b230a6c6ccff6edb7fd96f5e6
electricface <songwentai@deepin.com>
2017-09-11 15:07:39 +0800

    增加 RemoteLs()

--- flatpak.go
+++ flatpak.go
@@ -59,25 +59,41 @@ func GlDrivers() ([]string, error) {
 }
 
 type Ref struct {
+	Type   string
 	Name   string
 	Arch   string
 	Branch string
 }
 
-func (r *Ref) String() string {
+func (r Ref) String() string {
+	if r.Type != "" {
+		return fmt.Sprintf("%s/%s/%s/%s", r.Type, r.Name, r.Arch, r.Branch)
+	}
 	return fmt.Sprintf("%s/%s/%s", r.Name, r.Arch, r.Branch)
 }
 
 func parseRef(ref string) (Ref, error) {
 	parts := strings.Split(ref, "/")
-	if len(parts) != 3 {
-		return Ref{}, errors.New("length of parts is not 3")
+	fmt.Printf("parseRef parts: %#v\n", parts)
+	if len(parts) == 3 {
+		return Ref{
+			Name:   parts[0],
+			Arch:   parts[1],
+			Branch: parts[2],
+		}, nil
+	} else if len(parts) == 4 {
+		if parts[0] != "app" && parts[0] != "runtime" {
+			return Ref{}, fmt.Errorf("unknown type %q in ref %q", parts[0], ref)
+		}
+
+		return Ref{
+			Type:   parts[0],
+			Name:   parts[1],
+			Arch:   parts[2],
+			Branch: parts[3],
+		}, nil
 	}
-	return Ref{
-		Name:   parts[0],
-		Arch:   parts[1],
-		Branch: parts[2],
-	}, nil
+	return Ref{}, errors.New("length of parts is not 3 or 4")
 }
 
 type ListResult struct {
@@ -391,5 +407,111 @@ func Remotes(opts *RemotesOptions) ([]*RemoteRepo, error) {
 		}
 
 	}
+	return results, nil
+}
+
+type RemoteLsItem struct {
+	Ref           Ref
+	Commit        string
+	InstalledSize string
+	DownloadSize  string
+}
+
+type RemoteLsOptions struct {
+	Arch    string
+	User    bool
+	System  bool
+	Runtime bool
+	App     bool
+
+	Updates bool
+}
+
+func (o *RemoteLsOptions) getArgs() (args []string) {
+	if o == nil {
+		return nil
+	}
+	if o.Arch != "" {
+		args = append(args, "--arch"+o.Arch)
+	}
+	if o.User {
+		args = append(args, "--user")
+	}
+	if o.System {
+		args = append(args, "--system")
+	}
+	if o.Runtime {
+		args = append(args, "--runtime")
+	}
+	if o.App {
+		args = append(args, "--app")
+	}
+
+	if o.Updates {
+		args = append(args, "--updates")
+	}
+
+	return args
+}
+
+func parseRemoteLs(line string) (*RemoteLsItem, error) {
+	fmt.Printf("parseRemoteLs %q\n", line)
+	parts := regSpaces.Split(line, -1)
+	fmt.Printf("parseRemoteLs parts: %#v\n", parts)
+	if len(parts) < 6 {
+		return nil, errors.New("length of parts is less then 6")
+	}
+
+	ref, err := parseRef(parts[0])
+	if err != nil {
+		return nil, err
+	}
+	return &RemoteLsItem{
+		Ref:           ref,
+		Commit:        parts[1],
+		InstalledSize: parts[2] + " " + parts[3],
+		DownloadSize:  parts[4] + " " + parts[5],
+	}, nil
+}
+
+func RemoteLs(remote string, opts *RemoteLsOptions) (results []*RemoteLsItem, err error) {
+	args := []string{"remote-ls", remote, "-d"}
+	optArgs := opts.getArgs()
+	args = append(args, optArgs...)
+
+	cmd := exec.Command(flatpakBin, args...)
+	var stderrBuf bytes.Buffer
+	cmd.Stderr = &stderrBuf
+	stdoutPipe, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+	defer stdoutPipe.Close()
+
+	err = cmd.Start()
+	if err != nil {
+		return nil, err
+	}
+	stdoutReader := bufio.NewReader(stdoutPipe)
+	for {
+		bytes, err := stdoutReader.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+
+		result, err := parseRemoteLs(string(bytes))
+		if err != nil {
+			return nil, err
+		}
+
+		results = append(results, result)
+	}
+	err = cmd.Wait()
+	if err != nil {
+		return nil, wrapErrorCmdRun(err, &stderrBuf)
+	}
+
 	return results, nil
 }
